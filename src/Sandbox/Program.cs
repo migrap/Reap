d5f5f8995b6a8cefa752927ff80fb04c60d02f14
Michael Park <migrap@gmail.com>
2015-06-09 21:10:55 -0700

    Methoding

--- src/Sandbox/Program.cs
+++ src/Sandbox/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Reap;
 using Reap.Extensions.Authentication;
@@ -15,60 +19,17 @@ namespace Sandbox {
         public void Main(string[] args) {
             var settings = new MessageSerializerSettings();
             settings.Converters.Add(new MessageConverter());
-            settings.Converters.Add(new VersionConverter());
+            //settings.Converters.Add(new VersionConverter());
+
+            (new AccountService()).SendAsync(x => x.Create("Michael", "Checking"));
 
             var message = new Message();
-
-            //var authorization = message.Extension(x => x.Authorization, x => {
-            //});
-
-            //var headers = message.Extension(x => x.Headers, x => {
-            //    x.Headers.Add("", "");
-            //});
-
-            //var claims = message.Extension(x => x.Claims, x => {
-            //    x.Claims.Add("issuer", "type", "value");
-            //});
-
-            //var authentication = message.Extension(x => x.Authentication, x => {
-            //    x.Token = "Bearer 0xABCDEF0123456789";
-            //});
-
-            //var version = message.Extension(x => x.Version, x => {
-            //    x.Version = "1.0.0";
-            //});
 
             var mood = message.Extension(x => x.Mood, x => {
                 x.Mood = Mood.Happy;
             });
 
-            //var uri = message.Extension(x => x.Resource, x => {
-            //    x.Resource = "http://www.google.com";
-            //});
-
-            //var obj = message.Extension(x => x.Object, x => {
-            //    x.Class = "Person";
-            //    x.Properties = new {
-            //        name = "Michael",
-            //        age = 10,
-            //    };
-            //    x.Links.Add(Link.About("http://www.about.com"));
-            //    x.Title = "Person Description";
-            //});
-
-            //var urn = message.Extension(x => x.Resource, "urn:here@home.com");
-
-            //var urh = message.Extension(x => x.Resource, x => x.Home);
-
-            //var cmd = message.Extension(x => x.Command, x => {
-            //    x = x.CreateAccount(name: "Michael");
-            //});
-
-            //message.xExtension(x => x.Command, x => x.Command = Account.Create(create => {
-            //    create.Name = Guid.Empty.ToString();
-            //}));
-
-            var owner = Guid.NewGuid().ToString();
+            var owners = Enumerable.Range(0, 3).Select(x => Guid.NewGuid().ToString()).ToArray();
             var uuid = Guid.NewGuid().ToString();
 
             message.Extension(x => x.Command, x => {
@@ -76,30 +37,64 @@ namespace Sandbox {
                 x.Uri = "/account";
                 x.Name = "create";
                 x.Type = "application/vnd.bacnking+json";
-                x.Data = new { owner = owner, name = "Family Checking" };
+                x.Data = new { owners = owners, name = "Family Checking" };
             });
-
-            //message.Extension(x => x.Event, x => {
-            //    x.Path = "/account";
-            //    x.Name = "created";
-            //    x.Type = "application/vnd.bacnking+json";
-            //    x.Body = new {
-            //        owner = owner,
-            //        uuid = uuid,
-            //        name = "Family Checking",
-            //        date = DateTimeOffset.UtcNow,
-            //        balance = 0.0
-            //    };
-            //});
 
             var json = (string)null;
             json = JsonConvert.SerializeObject(message, settings);
+
+            
 
             //var egassem = JsonConvert.DeserializeObject<Message>(json, settings);
             //authorization = egassem.Extension(x => x.Authorization);
             //version = egassem.Extension(x => x.Version);
 
             //var contains = egassem.Extensions.Contains(x => x.Authentication, x => x.Jello);
+        }
+    }
+
+    public interface IAccountService {
+        Task SendAsync(Message message);
+    }
+
+    public class AccountService : IAccountService {
+        public Task SendAsync(Message message) {
+            return Task.FromResult(1);
+        }
+    }
+
+    public static class AccountServiceExtensions {
+        public static void SendAsync(this IAccountService account, Expression<Action<IAccountService>> expression) {
+            var mce = (MethodCallExpression)expression.Body;
+            var parameters = mce.Method.GetParameters();
+            var arguments = mce.Arguments.ToArray();
+
+            var data = ToDictionary(parameters.Select(x => x.Name).Skip(1), arguments.Skip(1));
+
+            var message = new Message();
+            message.Extension(x => x.Command, x => {
+                x.Uuid = Guid.NewGuid().ToString();
+                x.Uri = "/account";
+                x.Name = mce.Method.Name;
+                x.Type = "application/vnd.bacnking+json";
+                x.Data = data;
+            });
+            account.SendAsync(message);
+        }
+
+        public static void Create(this IAccountService account, string name, string type) {
+        }
+
+        private static Dictionary<string,object> ToDictionary(IEnumerable<string> keys, IEnumerable<object> values) {
+            var k = keys.GetEnumerator();
+            var v = values.GetEnumerator();
+            var d = new Dictionary<string, object>();
+
+            while(k.MoveNext()) {
+                d[k.Current] = (v.MoveNext()) ? v.Current : null;
+            }
+
+            return d;
         }
     }
 
@@ -111,30 +106,25 @@ namespace Sandbox {
         object Data { get; set; }
     }
 
+    public class CommandExtension : ICommandExtension {
+        public object Data { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Uri { get; set; }
+
+        public string Uuid { get; set; }
+    }
+
     public static class CommnadExtensions {
         public static ICommandExtension Extension(this Message message, ExtensionSelector<ICommandExtension> extension, Action<ICommandExtension> callback = null) {
             return message.Extension<ICommandExtension>(extension, callback);
         }
 
         public static ICommandExtension Command(this Message message) {
-            return message.Extension<ICommandExtension>();
+            return message.Extension<ICommandExtension>(new CommandExtension());
         }
     }
-
-    //public interface IEventExtension {
-    //    string Path { get; set; }
-    //    string Name { get; set; }
-    //    string Type { get; set; }
-    //    object Body { get; set; }
-    //}
-
-    //public static class EventExtensions {
-    //    public static IEventExtension Extension(this Message message, ExtensionSelector<IEventExtension> extension, Action<IEventExtension> callback = null) {
-    //        return message.Extension<IEventExtension>(extension, callback);
-    //    }
-
-    //    public static IEventExtension Event(this Message message) {
-    //        return message.Extension<IEventExtension>();
-    //    }
-    //}
 }
